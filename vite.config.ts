95621b3c564ae0b5d966599c4be93a68d663ab20
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2023-09-21 16:54:27 +0600

    root import alias fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tsconfigPaths(),
+    react({
+      exclude: ["**/node_modules/**", "**/vite.config.ts"],
+      include: [
+        "./vite.config.ts",
+        "src/**/*.tsx",
+        "src/**/*.ts",
+        "src/**/*.jsx",
+        "src/**/*.js",
+      ],
+    }),
+  ],
+});
