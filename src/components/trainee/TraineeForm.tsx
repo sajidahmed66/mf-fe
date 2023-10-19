0ac4b06496f2844039a5109b4aa9431ffa2cee27
sajid <sajidahmedsiddiqui66@gmail.com>
2023-10-19 08:50:42 +0600

    edit form wip

--- src/components/trainee/TraineeForm.tsx
+++ src/components/trainee/TraineeForm.tsx
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { useForm } from "@mantine/form";
 import { Box, NumberInput, TextInput, Fieldset, Radio, Group, Button } from "@mantine/core";
+import { validateName } from "@/libs/validators";
 interface ICreateTraineeFormValues {
   firstName: string;
   lastName: string;
@@ -28,24 +29,30 @@ const TraineeForm: React.FC<ITraineeForm> = ({ initialValues }) => {
       subscriptionType: '',
       totalAmount: 0
     },
+
+    validate: {
+      firstName: (value) => validateName(value) ? null : "name must"
+    }
   });
 
-  const handleSubmit = createTraineeForm.onSubmit(values => console.log({ values }))
+  const handleSubmit = createTraineeForm.onSubmit(values => console.log(values));
+
   useLayoutEffect(() => {
     if (initialValues) {
       createTraineeForm.setValues(initialValues)
       createTraineeForm.resetDirty(initialValues) // iTODO  do not know what it does need to figure out before i ship
     }
-  }, [initialValues, createTraineeForm])
+  }, [initialValues, createTraineeForm]);
+
   return (
-    <Box mx='auto' miw={320} >
+    <Box mx='auto' miw={300} className="sm:space-y-4 md:space-y-6" >
       <Fieldset legend="Member information">
         <TextInput label="First Name" placeholder="First Name" {...createTraineeForm.getInputProps('firstName')} />
         <TextInput label="Last Name" placeholder="Last Name" {...createTraineeForm.getInputProps('lastName')} />
         <TextInput label="Phone Number" placeholder="Enter your phone number" {...createTraineeForm.getInputProps('mobileNumber')} />
       </Fieldset>
-      <Fieldset legend="Registration details" >
 
+      <Fieldset legend="Registration details" >
         <NumberInput label="Registration Fee" allowNegative={false} allowDecimal={false} placeholder="Registration fee" min={0} {...createTraineeForm.getInputProps("registrationFee")} />
         <Radio.Group
           name="favoriteFramework"
@@ -59,6 +66,7 @@ const TraineeForm: React.FC<ITraineeForm> = ({ initialValues }) => {
           </Group>
         </Radio.Group>
       </Fieldset>
+
       <Fieldset legend="Payment Details" >
         <NumberInput label="Total Amount" allowNegative={false} allowDecimal={false} placeholder="Registration fee" min={0} {...createTraineeForm.getInputProps("totalAmount")} />
         <NumberInput label="Paid Amount" allowNegative={false} allowDecimal={false} placeholder="Registration fee" min={0} {...createTraineeForm.getInputProps("paidAmount")} />
--- src/libs/validators/index.ts
+++ src/libs/validators/index.ts
@@ -2,12 +2,28 @@ import * as yup from "yup";
 
 export const validatePhoneNumber = (value: string) => {
   const phoneRegex = /^(?:\+?88)?01[3-9]\d{8}$/;
-  let matcher = yup.string().matches(phoneRegex);
+  const matcher = yup.string().matches(phoneRegex);
   return matcher.isValidSync(value);
 };
 
 export const validatePassword = (value: string) => {
   // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-  let matcher = yup.string().min(6);
+  const matcher = yup.string().min(6);
   return matcher.isValidSync(value);
 };
+
+export const validateName = (value: string) => {
+  const matcher = yup.string().min(3);
+  return matcher.isValidSync(value);
+}
+
+export const validateNumber = (value: number) => {
+  const matcher = yup.number().min(1);
+  return matcher.isValidSync(value);
+};
+
+export const validatePackageType = (value: string) => {
+  const matcher = yup.mixed().oneOf(['monthly', 'package']);
+  return matcher.isValidSync(value);
+
+}
