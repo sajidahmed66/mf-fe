eb35b07e0583743722015eb8a03f2635cf833b35
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2023-12-25 19:53:54 +0600

    expense added  not tested

--- src/features/expense/expenseAPI.ts
+++ src/features/expense/expenseAPI.ts
@@ -1,11 +1,52 @@
 import apiSlice from "@/features/api/apiSlice";
-
+import apiRoutes from "@/libs/routepaths/apiroutes";
+// TODO finish expense
 const expenseAPI = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getExpenses: builder.query({
       query: () => ({
-        url: "/expenses",
+        url: apiRoutes.expenses,
+        method: "GET",
       }),
+      providesTags: ["Expense"],
+    }),
+    getExpensesById: builder.query({
+      query: (id: string) => ({
+        url: apiRoutes.expenseById(id),
+        method: "GET",
+      }),
+      providesTags: ["Expense"],
+    }),
+    createExpense: builder.mutation<any, any>({
+      query: (data) => ({
+        url: apiRoutes.expenses,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Expense"],
+    }),
+    updateExpense: builder.mutation<any, { data: any; id: string }>({
+      query: ({ data, id }) => ({
+        url: apiRoutes.expenseById(id),
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Expense"],
+    }),
+    deleteExpense: builder.mutation({
+      query: (id: string) => ({
+        url: apiRoutes.expenseById(id),
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Expense"],
     }),
   }),
 });
+
+export const {
+  useGetExpensesQuery,
+  useGetExpensesByIdQuery,
+  useCreateExpenseMutation,
+  useUpdateExpenseMutation,
+  useDeleteExpenseMutation,
+} = expenseAPI;
